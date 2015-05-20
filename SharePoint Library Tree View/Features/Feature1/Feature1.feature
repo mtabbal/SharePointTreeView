1a6dc42794854b48052dec797b1d6b79f28e34fe
Ravin Singh D <ravinsinghd@outlook.com>
2015-05-20 17:14:06 +0530

    Initial Commit

---
+++ SharePoint Library Tree View/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="a7c6b4d5-13a9-4368-abff-dcbb67f065ac" description="SharePoint App Feature" featureId="a7c6b4d5-13a9-4368-abff-dcbb67f065ac" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="SharePoint Library Tree View Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="90bc5467-3194-437b-8eeb-f18327f144c8" />
+    <projectItemReference itemId="22383b9d-6f21-471a-ae2c-3db0c2fe3b86" />
+    <projectItemReference itemId="757bda3b-a8f5-43df-a8b9-cd0850a15682" />
+    <projectItemReference itemId="f75a4569-74ec-4a6d-93d8-a62c4ff6e66b" />
+  </projectItems>
+</feature>
---
+++ SharePoint Library Tree View/pkg/Debug/SharePoint_Library_Tree_View/SharePoint Library Tree View_Feature1/Scripts/App.js
@@ -0,0 +1,114 @@
+﻿'use strict';
+
+var context = SP.ClientContext.get_current();
+var user = context.get_web().get_currentUser();
+var ExceptLiarray = ['Site Assets', 'Style Library', 'Form Templates'];
+// This code runs when the DOM is ready and creates a context object which is needed to use the SharePoint object model
+$(document).ready(function () {
+    $('#DocTree').jstree({
+        "core": {
+            'check_callback': true,
+        }
+    });
+    $("#DocTree").bind('select_node.jstree', function (e,data) {
+        var value = $("#DocTree").jstree("get_selected");
+        FetchSubFolder(data.node.data.RURL, value);
+        FetchFiles(data.node.data.RURL, value);
+    });
+    InitialCall();
+});
+function InitialCall() {
+    FetchAllLibrary().promise().then(
+            function (Libdata) {
+                $.each(Libdata.d.results, function (key, value) {
+                    if (ExceptLiarray.indexOf(value.Title) == -1) {
+                        FetchLibRelativeURL(value.RootFolder.__deferred.uri).promise().then(
+                            function (data) {
+                                AddNoteToTree(null,data.d.Name, encodeURI(data.d.ServerRelativeUrl),'fi-folder');
+                            },
+                            function (err) {
+                            }
+                        );
+                    }
+                });
+            },
+            function (err) {
+            }
+            );
+}
+function FetchAllLibrary() {
+    var deferred = $.Deferred();
+    $.ajax({
+        url: _spPageContextInfo.siteAbsoluteUrl + '/_api/web/lists?$filter=BaseTemplate  eq 101',
+        type: "GET",
+        headers: { "Accept": "application/json;odata=verbose" },
+        success: function (data) {
+            deferred.resolve(data);
+        },
+        error: function (data2) {
+            self.error("Error in processing request " + data2.success);
+        }
+    });
+    return deferred;
+}
+function FetchLibRelativeURL(URL) {
+    var deferred = $.Deferred();
+    $.ajax({
+        url: URL,
+        type: "GET",
+        headers: { "Accept": "application/json;odata=verbose" },
+        success: function (data) {
+            deferred.resolve(data);
+        },
+        error: function (data2) {
+        }
+    });
+    return deferred;
+}
+function FetchSubFolder(ServerRelativeURL,ParentNode) {
+    $.ajax({
+        url: _spPageContextInfo.siteAbsoluteUrl + "/_api/web/GetFolderByServerRelativeUrl('" + ServerRelativeURL + "')/Folders",
+        type: "GET",
+        headers: { "Accept": "application/json;odata=verbose" },
+        async: false,
+        success: function (data) {
+            $.each(data.d.results, function (key, value) {
+                if (value.Name != 'Forms') {
+                    AddNoteToTree(ParentNode, value.Name, value.ServerRelativeUrl,'fi-folder');
+                }
+            });
+        },
+        error: function (data2) {
+        }
+    });
+}
+function FetchFiles(ServerRelativeURL, ParentNode) {
+    $.ajax({
+        url: _spPageContextInfo.siteAbsoluteUrl + "/_api/web/GetFolderByServerRelativeUrl('" + ServerRelativeURL + "')/Files",
+        type: "GET",
+        headers: { "Accept": "application/json;odata=verbose" },
+        async: false,
+        success: function (data) {
+            $.each(data.d.results, function (key, value) {
+                var FileLink = '<a href="' + value.ServerRelativeUrl + '">' + value.Name + '</a>';
+                AddNoteToTree(ParentNode, FileLink, value.ServerRelativeUrl, 'fi-page');
+            });
+        },
+        error: function (data2) {
+        }
+    });
+}
+function AddNoteToTree(ParentNode, NodeName, RelativeURL,IconName) {
+    var id = $("#DocTree").jstree('create_node', ParentNode,
+        {
+            "data": { "RURL": RelativeURL },
+            "text": NodeName,
+            "icon": IconName,
+            'children': false,
+        },
+'last');
+}
+
+
+
+
